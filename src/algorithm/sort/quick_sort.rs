24fb99620793796c6a38cd0b9b3a893139cc0b16
Stephen Myers <scmyers11@gmail.com>
2022-08-09 21:05:28 -0500

    Break in the error case to prevent edge case problems

--- src/algorithm/sort/quick_sort.rs
+++ src/algorithm/sort/quick_sort.rs
@@ -87,10 +87,11 @@ impl<T: Float> QuickArgSort for Vec<T> {
                     if j < i {
                         break;
                     }
-                    if i < self.len() && j < self.len() {
-                        self.swap(i, j);
-                        index.swap(i, j);
+                    if i > self.len() || j > self.len() {
+                        break;
                     }
+                    self.swap(i, j);
+                    index.swap(i, j);
                 }
                 self[l + 1] = self[j];
                 self[j] = a;
